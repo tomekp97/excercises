31761d2fa7b7d8db126302996c0be0a55036f2f6
Tomek <tompe97@tlen.pl>
2019-06-24 07:40:34 +0100

    WIP Add new puzzle

---
+++ xmas-pudding.py
@@ -0,0 +1,20 @@
+"""
+https://www.mathsisfun.com/puzzles/christmas-pudding.html
+
+Father Christmas spends 364 days of the year as a taster of Christmas puddings (which is why he is so round and jolly). Recently he came across a Magic Pudding.
+
+If you eat some of this pudding, the next thing you say comes out in a magic code.
+
+Father Christmas tried it, liked it, and said:
+
+"IP! IP! IP! XIBU B MPWFMZ QVEEJOH! NFSSZ DISJTUNBT BOE B IBQQZ OFX ZFBS UP ZPV BMM!"
+"""
+
+import string
+
+sentence = "IP! IP! IP! XIBU B MPWFMZ QVEEJOH! NFSSZ DISJTUNBT BOE B IBQQZ OFX ZFBS UP ZPV BMM!"
+
+def decrypt(sentence, offset):
+    pass
+
+decrypt()
